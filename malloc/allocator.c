92e03237188c52db046eb4ca7645f6ccdb189018
quang1610 <nguyenqu2@grinnell.edu>
2020-02-21 15:38:41 -0600

    partially solve lab

--- malloc/allocator.c
+++ malloc/allocator.c
@@ -17,12 +17,63 @@
 // The size of a single page of memory, in bytes
 #define PAGE_SIZE 0x1000
 
+// function prototypes
+int round_up_multiple_16(int size);
+int find_free_lists_index (size_t size);
+void* xxmalloc(size_t size);
+void xxfree(void* ptr);
+size_t xxmalloc_usable_size(void* ptr);
+
+// support data structures
+typedef struct header {
+  size_t size;
+  long magic_number;
+} header_t;
+
+typedef struct node {
+  struct node* next;
+} node_t;
+
+node_t* free_linked_lists[8];
+
+//****************************************************************************************************************
+/**
+ * Initialized chunks and blocks of memory inside each chunk.
+ */
+__attribute__((constructor)) void init_heap() {
+  size_t current_size = MIN_MALLOC_SIZE / 2;
+  
+  for (int i = 0; i < 8; i ++) {
+    current_size *= 2;
+    void * new_chunk =  mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+
+    /// initialize the header
+    header_t* head = (header_t*) new_chunk;
+    head->size = current_size;
+    head->magic_number = (long) current_size;
+
+    /// allocate the free linked list
+    /// set the array elem point to first elem of free linked list
+    free_linked_lists[i] = (node_t*) ((uintptr_t)new_chunk + current_size);
+    node_t* cursor = free_linked_lists[i];
+    long n_loops = (long) (PAGE_SIZE/current_size - 1);
+    printf ("Debug, Allocating chunk for %lu, looping %lu time\n", current_size, n_loops);
+    
+    for (int i = 0; i < n_loops - 1; i ++) {
+      cursor->next = (node_t*) ((uintptr_t)cursor + current_size);
+      cursor = cursor->next;
+    }
+    cursor->next = NULL;
+  }
+}
+
+//****************************************************************************************************************
 /**
  * Calculate the object size based on the input integer. The return value should be a multiple of 16 and
  * exponent of 2 and smallest possible such that it is >= input integer
  */
 int round_up_multiple_16(int size) {
-  int return_value = 16;
+  int return_value = MIN_MALLOC_SIZE;
   while (size > return_value) {
     return_value *= 2;
   }
@@ -30,18 +81,19 @@ int round_up_multiple_16(int size) {
 }
 
 /**
- * find_free_list_ind_base_16 calculate which type of object does input size fall into. if size <= 16 then 
+ * find_free_list_type calculate which type of object does input size fall into. if size <= 16 then 
  * return 0. If 16 < size <= 32, return 1. If 32 < size <= 64, return 2...
  */
-int find_free_list_ind_base16 (int size) {
+int find_free_lists_index (size_t size) {
   int return_value = 0;
-  while (size > 16) {
+  while (size > MIN_MALLOC_SIZE) {
     size = size / 2;
     return_value ++;
   }
   return return_value;
 }
 
+//****************************************************************************************************************
 /**
  * Allocate space on the heap.
  * \param size  The minimium number of bytes that must be allocated
@@ -49,19 +101,21 @@ int find_free_list_ind_base16 (int size) {
  *              This function may return NULL when an error occurs.
  */
 void* xxmalloc(size_t size) {
-  // Round the size up to the next multiple of the page size
-  size = ROUND_UP(size, PAGE_SIZE);
-  
-  // Request memory from the operating system in page-sized chunks
-  void* p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  if (size > PAGE_SIZE/2) {
+    /// allocate object > 2048
+    // Round the size up to the next multiple of the page size
+    size = ROUND_UP(size, PAGE_SIZE);
+    return mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+  } else {
+    /// allocate object <= 2048
+    int free_linked_lists_index = find_free_lists_index(size);
+    node_t* p = free_linked_lists[free_linked_lists_index];
 
-  // Check for errors
-  if(p == MAP_FAILED) {
-    fputs("mmap failed! Giving up.\n", stderr);
-    exit(2);
-  }
+    // remove the list from the current linked_list
+    if (p != NULL) free_linked_lists[free_linked_lists_index] = p -> next;
   
-  return p;
+    return p;
+  }
 }
 
 /**
@@ -71,8 +125,17 @@ void* xxmalloc(size_t size) {
 void xxfree(void* ptr) {
   // Don't free NULL!
   if(ptr == NULL) return;
-  
-  // TODO: Complete this function
+
+  /// find the chunk
+  size_t object_size = xxmalloc_usable_size(ptr);
+
+  if (object_size != 0) {
+    int free_linked_lists_index = find_free_lists_index(object_size);
+
+    node_t* freed_node = (node_t*) ptr;
+    freed_node->next = free_linked_lists[free_linked_lists_index]->next;
+    free_linked_lists[free_linked_lists_index] = freed_node;
+  }
 }
 
 /**
@@ -81,7 +144,13 @@ void xxfree(void* ptr) {
  * \returns     The number of bytes available for use in this object
  */
 size_t xxmalloc_usable_size(void* ptr) {
-  // TODO: Complete this function
-  return 0;
+  header_t* head = (header_t*) ((uintptr_t)ptr - ((uintptr_t)ptr % PAGE_SIZE));
+  size_t object_size = head->size;
+
+  if(head->magic_number == (long)object_size) {
+    return object_size;
+  } else {
+    return 0;
+  }
 }
 
