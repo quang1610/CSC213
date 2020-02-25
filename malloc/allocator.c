638975b17eebabc9fe8b3444d441fe85847b79bf
quang1610 <nguyenqu2@grinnell.edu>
2020-02-25 14:35:41 -0600

    working on the malloc problem

--- malloc/allocator.c
+++ malloc/allocator.c
@@ -18,7 +18,7 @@
 #define PAGE_SIZE 0x1000
 
 // function prototypes
-int round_up_multiple_16(int size);
+size_t round_up_x16(size_t size);
 int find_free_lists_index (size_t size);
 void* xxmalloc(size_t size);
 void xxfree(void* ptr);
@@ -41,12 +41,12 @@ node_t* free_linked_lists[8];
  * Initialized chunks and blocks of memory inside each chunk.
  */
 __attribute__((constructor)) void init_heap() {
-  size_t current_size = MIN_MALLOC_SIZE / 2;
+  int current_size = MIN_MALLOC_SIZE / 2;
   
   for (int i = 0; i < 8; i ++) {
     current_size *= 2;
+    
     void * new_chunk =  mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-
     /// initialize the header
     header_t* head = (header_t*) new_chunk;
     head->size = current_size;
@@ -54,13 +54,13 @@ __attribute__((constructor)) void init_heap() {
 
     /// allocate the free linked list
     /// set the array elem point to first elem of free linked list
-    free_linked_lists[i] = (node_t*) ((uintptr_t)new_chunk + current_size);
+    free_linked_lists[i] = (node_t*) ((char*)new_chunk + current_size);
     node_t* cursor = free_linked_lists[i];
-    long n_loops = (long) (PAGE_SIZE/current_size - 1);
-    printf ("Debug, Allocating chunk for %lu, looping %lu time\n", current_size, n_loops);
+    long n_loops = (long) (PAGE_SIZE/current_size);
+    printf ("Debug, Allocating chunk for %d, looping %lu time\n", current_size, n_loops);
     
-    for (int i = 0; i < n_loops - 1; i ++) {
-      cursor->next = (node_t*) ((uintptr_t)cursor + current_size);
+    for (long j = 2; j < n_loops; j ++) {
+      cursor->next = (node_t*) ((char*)cursor + current_size);
       cursor = cursor->next;
     }
     cursor->next = NULL;
@@ -72,8 +72,8 @@ __attribute__((constructor)) void init_heap() {
  * Calculate the object size based on the input integer. The return value should be a multiple of 16 and
  * exponent of 2 and smallest possible such that it is >= input integer
  */
-int round_up_multiple_16(int size) {
-  int return_value = MIN_MALLOC_SIZE;
+size_t round_up_x16(size_t size) {
+  size_t return_value = MIN_MALLOC_SIZE;
   while (size > return_value) {
     return_value *= 2;
   }
@@ -85,9 +85,15 @@ int round_up_multiple_16(int size) {
  * return 0. If 16 < size <= 32, return 1. If 32 < size <= 64, return 2...
  */
 int find_free_lists_index (size_t size) {
+  size = round_up_x16(size);
   int return_value = 0;
+  
   while (size > MIN_MALLOC_SIZE) {
-    size = size / 2;
+    if (size % 2 == 0) {
+      size = size / 2;
+    } else {
+      size = size / 2 + 1;
+    }
     return_value ++;
   }
   return return_value;
@@ -108,13 +114,42 @@ void* xxmalloc(size_t size) {
     return mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
   } else {
     /// allocate object <= 2048
+    size = round_up_x16(size);
     int free_linked_lists_index = find_free_lists_index(size);
     node_t* p = free_linked_lists[free_linked_lists_index];
 
     // remove the list from the current linked_list
-    if (p != NULL) free_linked_lists[free_linked_lists_index] = p -> next;
-  
-    return p;
+    if (p != NULL) {
+      free_linked_lists[free_linked_lists_index] = p -> next;
+      return p;
+    } else {
+      /// realize that we run out of space, allocate using mmap
+      size = ROUND_UP(size, PAGE_SIZE);
+      void* new_chunk =  mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+
+      header_t *head = (header_t*) new_chunk;
+      head->size = size;
+      head->magic_number = (long) size;
+
+      /// allocate the free linked list
+      /// set the array elem point to first elem of free linked list
+      free_linked_lists[free_linked_lists_index] = (node_t*) ((char*)new_chunk + size);
+      node_t* cursor = free_linked_lists[free_linked_lists_index];
+      long n_loops = (long) (PAGE_SIZE/size - 2);
+      printf ("Debug, Allocating chunk for %zu, looping %lu time\n", size, n_loops);
+    
+      for (long j = 0; j < n_loops; j ++) {
+        cursor->next = (node_t*) ((char*)cursor + size);
+        cursor = cursor->next;
+      }
+      cursor->next = NULL;
+
+      /// set return value
+      p = free_linked_lists[free_linked_lists_index];
+      free_linked_lists[free_linked_lists_index] = p->next;
+
+      return p;
+    }
   }
 }
 
@@ -144,7 +179,7 @@ void xxfree(void* ptr) {
  * \returns     The number of bytes available for use in this object
  */
 size_t xxmalloc_usable_size(void* ptr) {
-  header_t* head = (header_t*) ((uintptr_t)ptr - ((uintptr_t)ptr % PAGE_SIZE));
+  header_t* head = (header_t*) ((char*)ptr - ((uintptr_t)ptr % PAGE_SIZE));
   size_t object_size = head->size;
 
   if(head->magic_number == (long)object_size) {
