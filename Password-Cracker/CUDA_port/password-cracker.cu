88d197c2e3b1eb9806134f053381f271b7c59c2b
quang1610 <nguyenducquang1610@gmail.com>
2020-09-13 02:55:51 -0500

    porting md5 to cuda

--- Password-Cracker/CUDA_port/password-cracker.cu
+++ Password-Cracker/CUDA_port/password-cracker.cu
@@ -44,21 +44,22 @@ typedef struct password_set {
 
 /******************* Device code **************************/
 __global__ void single_crack_MD5(unsigned *input_hash, int *cracked, int id_offset) {
-    if (cracked == NOT_CRACKED) {
-        N = threadIdx.x + blockIdx.x * blockDim.x;
+    if (*cracked == NOT_CRACKED) {
+        int N = threadIdx.x + blockIdx.x * blockDim.x;
         if (N >= PASSWORD_SPACE_SIZE) {
             return;
         } 
 
-        unsigned char candidate_password = "aaaaaa";
+        // generate candidate based on N
+        char candidate_password[] = "aaaaaa";
         for (int j = PASSWORD_LENGTH - 1; j >= 0; j--) {
             candidate_password[j] = (char) ('a' + N % CHAR_NUM);
             N = N / CHAR_NUM;
         }
 
         // generate candidate hash
-        unsigned *candidate_hash = malloc(sizeof(unsigned) * 4);
-        md5(candidate_password, PASSWORD_LENGTH, candidate_hash));
+        unsigned *candidate_hash = (unsigned*) malloc(sizeof(unsigned) * 4);
+        md5(&candidate_password, PASSWORD_LENGTH, candidate_hash);
 
         // compare candidate hash with input hash
         for (int i = 0; i < 4; i++) {
@@ -79,14 +80,12 @@ void crack_single_password(unsigned *input_hash, char *output, *int cracked) {
     int block_size = 512;
 
     int total_thread = 0;
-    int *cracked = malloc(sizeof(int)); 
-    *cracked = NOT_CRACKED;
 
-    while total_thread < PASSWORD_SPACE_SIZE {
+    while (total_thread < PASSWORD_SPACE_SIZE) {
         if (*cracked == NOT_CRACKED) {
             <<<num_block, block_size>>>single_crack_MD5(input_hash, cracked, total_thread);
             cudaDeviceSynchronize();
-            
+
             total_thread += num_block * num_thread;
         } else {
             break;
@@ -176,65 +175,65 @@ int main(int argc, char **argv) {
         free(cracked);
 
     } else if (strcmp(argv[1], "list") == 0) {
-        // Make and initialize a password set
-        password_set_t passwords;
-        init_password_set(&passwords);
+    //     // Make and initialize a password set
+    //     password_set_t passwords;
+    //     init_password_set(&passwords);
 
-        // Open the password file
-        FILE *password_file = fopen(argv[2], "r");
-        if (password_file == NULL) {
-            perror("opening password file");
-            exit(2);
-        }
+    //     // Open the password file
+    //     FILE *password_file = fopen(argv[2], "r");
+    //     if (password_file == NULL) {
+    //         perror("opening password file");
+    //         exit(2);
+    //     }
 
-        int password_count = 0;
+    //     int password_count = 0;
 
-        // Read until we hit the end of the file
-        while (!feof(password_file)) {
-            // Make space to hold the username
-            char username[MAX_USERNAME_LENGTH];
+    //     // Read until we hit the end of the file
+    //     while (!feof(password_file)) {
+    //         // Make space to hold the username
+    //         char username[MAX_USERNAME_LENGTH];
 
-            // Make space to hold the MD5 string
-            char md5_string[MD5_UNSIGNED_HASH_LEN * 4 + 1];
+    //         // Make space to hold the MD5 string
+    //         char md5_string[MD5_UNSIGNED_HASH_LEN * 4 + 1];
 
-            // Make space to hold the MD5 bytes
-            unsigned password_hash[MD5_UNSIGNED_HASH_LEN];
+    //         // Make space to hold the MD5 bytes
+    //         unsigned password_hash[MD5_UNSIGNED_HASH_LEN];
 
-            // Try to read. The space in the format string is required to eat the newline
-            if (fscanf(password_file, "%s %s ", username, md5_string) != 2) {
-                fprintf(stderr, "Error reading password file: malformed line\n");
-                exit(2);
-            }
+    //         // Try to read. The space in the format string is required to eat the newline
+    //         if (fscanf(password_file, "%s %s ", username, md5_string) != 2) {
+    //             fprintf(stderr, "Error reading password file: malformed line\n");
+    //             exit(2);
+    //         }
 
-            // Convert the MD5 string to MD5 bytes in our new node
-            if (md5_string_to_unsigned(md5_string, password_hash) != 0) {
-                fprintf(stderr, "Error reading MD5\n");
-                exit(2);
-            }
+    //         // Convert the MD5 string to MD5 bytes in our new node
+    //         if (md5_string_to_unsigned(md5_string, password_hash) != 0) {
+    //             fprintf(stderr, "Error reading MD5\n");
+    //             exit(2);
+    //         }
 
-            // Add the password to the password set
-            add_password(&passwords, username, password_hash);
-            password_count++;
-        }
+    //         // Add the password to the password set
+    //         add_password(&passwords, username, password_hash);
+    //         password_count++;
+    //     }
 
 
-        // Now run the password list cracker
-        int cracked = crack_password_list(&passwords);
+    //     // Now run the password list cracker
+    //     int cracked = crack_password_list(&passwords);
 
-        printf("Cracked %d of %d passwords.\n", cracked, password_count);
+    //     printf("Cracked %d of %d passwords.\n", cracked, password_count);
 
-        // free passwords set
-        user_password_t *cursor = passwords.head;
-        user_password_t *temp = NULL;
-        while (cursor != NULL) {
-            temp = cursor;
-            cursor = cursor->next;
-            free(temp);
-        }
+    //     // free passwords set
+    //     user_password_t *cursor = passwords.head;
+    //     user_password_t *temp = NULL;
+    //     while (cursor != NULL) {
+    //         temp = cursor;
+    //         cursor = cursor->next;
+    //         free(temp);
+    //     }
 
-    } else {
-        print_usage(argv[0]);
-        exit(1);
-    }
+    // } else {
+    //     print_usage(argv[0]);
+    //     exit(1);
+    // }
     return 0;
 }
