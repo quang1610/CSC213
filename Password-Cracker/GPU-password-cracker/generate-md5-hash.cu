447c86037573a645a4886626ba06b0802e9bf58d
quang1610 <nguyenducquang1610@gmail.com>
2020-09-16 02:52:02 -0500

    fixing bug related to checking hashcode

--- Password-Cracker/GPU-password-cracker/generate-md5-hash.cu
+++ Password-Cracker/GPU-password-cracker/generate-md5-hash.cu
@@ -6,6 +6,35 @@
 
 #define PASSWORD_LENGTH 6
 
+/**
+ * Convert a string representation of an MD5 hash to a sequence
+ * of bytes. The input md5_string must be 32 characters long, and
+ * the output buffer bytes must have room for MD5_DIGEST_LENGTH
+ * bytes.
+ *
+ * \param md5_string  The md5 string representation
+ * \param bytes       The destination buffer for the converted md5 hash
+ * \returns           0 on success, -1 otherwise
+ */
+int md5_string_to_bytes(const char *md5_string, uint8_t *hash_code) {
+    // Check for a valid MD5 string
+    if (strlen(md5_string) != 2 * MD5_UNSIGNED_HASH_LEN) return -1;
+
+    // Start our "cursor" at the start of the string
+    const char *pos = md5_string;
+
+    // Loop until we've read enough bytes
+    for (size_t i = 0; i < MD5_UNSIGNED_HASH_LEN; i++) {
+        // Read one byte (two characters)
+        int rc = sscanf(pos, "%2hhx", &hash_code[i]);
+        if (rc != 1) return -1;
+
+        // Move the "cursor" to the next hexadecimal byte
+        pos += 2;
+    }
+    return 0;
+}
+
 typedef union hcu {
     unsigned char b[MD5_UNSIGNED_HASH_LEN];
 } HCunion;
@@ -17,6 +46,7 @@ __global__ void get_md5_hashcode(unsigned char *password, int password_len, uint
 }
 
 int main(int argc, char **argv) {
+    // check for valid arguments
     if (argc != 2) {
         printf("The program require an argument, a string, to work!\n");
         exit(1);
@@ -27,6 +57,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    // allocate memory for hashcode and password to be used in GPU mem
     uint8_t *hash_code;
     cudaMallocManaged(&hash_code, sizeof(uint8_t) * (MD5_UNSIGNED_HASH_LEN));
 
@@ -34,17 +65,18 @@ int main(int argc, char **argv) {
     cudaMalloc(&gpu_password, sizeof(char) * (PASSWORD_LENGTH + 1));
     cudaMemcpy(gpu_password, argv[1], sizeof(char) * (PASSWORD_LENGTH + 1), cudaMemcpyHostToDevice);
 
+    // set the hashcode into hash_code string.
     get_md5_hashcode<<<1,1>>>((unsigned char *) gpu_password, PASSWORD_LENGTH, hash_code);
     cudaDeviceSynchronize();
 
     // print the pass code in hex form
     HCunion h;
     memcpy(&h, hash_code, sizeof(unsigned char) * MD5_UNSIGNED_HASH_LEN);
-
+    char *temp_str = malloc(sizeof(char) * 33);
     for (int i = 0; i < MD5_UNSIGNED_HASH_LEN; i++) {
-        printf("%02x", h.b[i]);
+        sprintf(&(temp_str[i*2]), "%02x", h.b[i]);
     }
-    printf("\n");
+    printf("%s\n", temp_str);
 
     cudaFree(hash_code);
     cudaFree(gpu_password);
