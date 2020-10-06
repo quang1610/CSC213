bd6a2354402008eeb521aa1ead9d2780f33f98aa
quang1610 <nguyenducquang1610@gmail.com>
2020-10-06 07:28:08 -0500

    Improving password cracker

--- Password-Cracker/GPU-password-cracker/password-cracker.cu
+++ Password-Cracker/GPU-password-cracker/password-cracker.cu
@@ -31,7 +31,7 @@
  *      this decides the candidate password.
  */
 __global__ void single_crack_MD5(uint8_t *input_hash, char* output, int *cracked, int id_offset) {
-    __share__ 8uint_t s_input_hash[MD5_UNSIGNED_HASH_LEN];
+    __shared__ 8uint_t s_input_hash[MD5_UNSIGNED_HASH_LEN];
 
     // get N based on the number id of block. This is used to construct to candidate password.
     // N = 0 would give us "aaaaaa" 
